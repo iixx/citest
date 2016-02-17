ca06827729fb9678c1cf5e4c1e71c1b31a421ae3
song <musicsxy@gmail.com>
2016-02-17 13:00:49 +0800

    init

---
+++ src/main/java/org/xy/citest/BeanA.java
@@ -0,0 +1,22 @@
+package org.xy.citest;
+
+public class BeanA {
+
+    private int num;
+    private String msg;
+    
+    
+    public int getNum() {
+        return this.num;
+    }
+    public String getMsg() {
+        return this.msg;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+}
---
+++ src/test/java/junit/test1/TestA.java
@@ -0,0 +1,20 @@
+package junit.test1;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestA {
+    
+    @Test
+    public void test1() {
+        assertTrue(false);
+    }
+    
+    @Test
+    public void test2() {
+        System.out.println("this is test2 print");
+        assertFalse(false);
+    }
+}
